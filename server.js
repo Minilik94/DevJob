4e863fefe3488183f91d4b22e26927be238ba187
minilik94 <minilikzeru@gmail.com>
2023-10-06 18:10:59 +0300

    Production Ready

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose
   .then(() => console.log("DB Connection successful!"));
 
   
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 app.listen(port, (req, res) => {
   console.log(`App running on port: ${port}`);
