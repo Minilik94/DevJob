8f906954802bb58475a3ae74fe431dfa566938f7
minilik94 <minilikzeru@gmail.com>
2023-10-07 11:02:56 +0300

    last but not least

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -2,6 +2,10 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			crawl: true,
+			entries: ['*']
+		}
 	}
 };
