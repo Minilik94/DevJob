89821c1ea555e3c6c58612054cb7e492929307ee
minilik94 <minilikzeru@gmail.com>
2023-11-09 20:20:17 +0300

    render latest

--- client/src/routes/+page.js
+++ client/src/routes/+page.js
@@ -2,7 +2,7 @@
 import { error } from "@sveltejs/kit";
 
 export const  load = async ({fetch}) => {
-    const response = await fetch('http://127.0.0.1:3000/api/v1/jobs')
+    const response = await fetch('/api/v1/jobs')
 
     if(!response.ok || response.ok === undefined) {
         throw error(response.status, 'Something Went Wrong Please try again later ')
