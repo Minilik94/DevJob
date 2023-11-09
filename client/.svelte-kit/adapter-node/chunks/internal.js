49c206e37c337e427f373dac0be3ea7182afc193
minilik94 <minilikzeru@gmail.com>
2023-11-09 19:54:02 +0300

    final temporary vite update

--- client/.svelte-kit/adapter-node/chunks/internal.js
+++ client/.svelte-kit/adapter-node/chunks/internal.js
@@ -168,7 +168,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "190v4vq"
+  version_hash: "1rq7ie0"
 };
 function get_hooks() {
   return {};
--- client/.svelte-kit/output/server/nodes/2.js
+++ client/.svelte-kit/output/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/jobs/_id_/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.4673513f.js","_app/immutable/chunks/scheduler.50aa2f44.js","_app/immutable/chunks/index.e7946bc4.js","_app/immutable/chunks/stores.c3e4b05a.js","_app/immutable/chunks/singletons.4672933d.js","_app/immutable/chunks/index.0362d59b.js"];
+export const imports = ["_app/immutable/nodes/2.0b043b90.js","_app/immutable/chunks/scheduler.50aa2f44.js","_app/immutable/chunks/index.e7946bc4.js","_app/immutable/chunks/stores.d48faa25.js","_app/immutable/chunks/singletons.f9e3830d.js","_app/immutable/chunks/index.0362d59b.js"];
 export const stylesheets = [];
 export const fonts = [];
