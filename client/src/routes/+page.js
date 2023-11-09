69a8e7418366ba8225213fe0dc4b2892d009194e
minilik94 <minilikzeru@gmail.com>
2023-11-09 20:24:00 +0300

    final render update

--- client/src/routes/+page.js
+++ client/src/routes/+page.js
@@ -2,6 +2,7 @@
 import { error } from "@sveltejs/kit";
 
 export const  load = async ({fetch}) => {
+    // const response = await fetch('http://127.0.0.1:3000/api/v1/jobs') only for development
     const response = await fetch('/api/v1/jobs')
 
     if(!response.ok || response.ok === undefined) {
