cb58462436489f39c053c5cc65593ca46b5a09f9
minilik94 <minilikzeru@gmail.com>
2023-11-09 19:41:18 +0300

    nowldy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [sveltekit()]
 });
